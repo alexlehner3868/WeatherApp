5b9996ffef746a5f90462adcf3b306c04ed6c458
Alex <alexlehner314@gmail.com>
2020-04-29 14:36:49 -0400

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Alexander Lehner on 2020-04-29.
+//  Copyright © 2020 LiveWeatherLove. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
