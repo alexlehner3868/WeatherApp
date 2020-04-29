7d2c213d316b5ad5430ac5d31e6ba07c257c35ec
Alex <alexlehner314@gmail.com>
2020-04-29 16:47:20 -0400

    Added temp images

---
+++ Weather/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Alexander Lehner on 2020-04-29.
+//  Copyright © 2020 LiveWeatherLove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
