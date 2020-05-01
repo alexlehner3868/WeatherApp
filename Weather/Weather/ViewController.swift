a9f7d2d3d8f68bd9bbb96390f739da2cb28fbecb
Alex <alexlehner314@gmail.com>
2020-05-01 00:11:21 -0400

    Added function to change units from F to C depending on location

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -7,13 +7,132 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
+import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let apiKey = ""
+    var lat = 11.344533
+    var lon = 104.33322
+    var activityIndicator: NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
+  
+    
+    //Connects Items to story board
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var contitionImageView: UIImageView!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var unitLabel: UILabel!
+    
+    let gradientLayer = CAGradientLayer()
+      
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        backgroundView.layer.addSublayer(gradientLayer)
+        
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        //getting user location
+        locationManager.requestWhenInUseAuthorization()
+        
+        activityIndicator.startAnimating()
+         if(CLLocationManager.locationServicesEnabled()){
+                   locationManager.delegate = self
+                   locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                   locationManager.startUpdatingLocation()
+               }
+
+        
+
     }
+    
+    //updating location
+     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+         Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
+                   response in
+                   self.activityIndicator.stopAnimating()
+                   if let responseStr = response.result.value {
+                let jsonResponse = JSON(responseStr)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonWeather["icon"].stringValue
+            
+                    //for icon value ensure that we name the images like they are named now cuz thats how api return them
+                let country = jsonResponse["sys"].array![3].stringValue
+                    
+                var temperature = Int(round(jsonTemp["temp"].doubleValue))
+                var units = "°C"
+                    
+                if(country == "US"){
+                    temperature = (temperature * 9 / 5) + 32
+                    units = "°F"
+                }
+                    //here is where we would get the other pieces of info on weather to calculate what to wear
+                //to see what other info is available put the url in google and replace lat, lon, and api with values
+
+                    //updating elements on screen with new values
+                    self.locationLabel.text = jsonResponse["name"].stringValue
+                    self.contitionImageView.image = UIImage(named: iconName)
+                    self.conditionLabel.text = jsonWeather["main"].stringValue
+                    self.temperatureLabel.text = "\(temperature)"
+                    self.unitLabel.text = "\(units)"
+                    
+                    let date = Date()
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "EEEE"
+                    self.dayLabel.text = dateFormatter.string(from: date)
+
+                    let suffix = iconName.suffix(1)
+                    if(suffix == "n"){
+                        self.setGreyGradientBackground()
+                    }else{
+                        self.setBlueGradientBackground()
+                    }
+            }
+        }
+        self.locationManager.stopUpdatingLocation()
+     }
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    //make the background appear like a gradient
+    override func viewWillAppear(_ animated: Bool) {
+           setBlueGradientBackground()
+       }
+
+    func setBlueGradientBackground(){
+           let topColor = UIColor(red: 95.0/255.0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
+           let bottomColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 184.0/255.0, alpha: 1.0).cgColor
+           gradientLayer.frame = view.bounds
+           gradientLayer.colors = [topColor, bottomColor]
+       }
+       
+       func setGreyGradientBackground(){
+           let topColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0).cgColor
+           let bottomColor = UIColor(red: 72.0/255.0, green: 72.0/255.0, blue: 72.0/255.0, alpha: 1.0).cgColor
+           gradientLayer.frame = view.bounds
+           gradientLayer.colors = [topColor, bottomColor]
+       }
 
 
 }
